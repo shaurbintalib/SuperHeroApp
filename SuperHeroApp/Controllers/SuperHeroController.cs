bc4c38061c0b60cb1d225c5f4608d75d7729ed1b
shaurbintalib <shaurbintalib515@gmail.com>
2025-03-08 13:59:46 +0800

    Super Hero all curd operations

--- SuperHeroApp/Controllers/SuperHeroController.cs
+++ SuperHeroApp/Controllers/SuperHeroController.cs
@@ -23,5 +23,42 @@ namespace SuperHeroApp.Controllers
             var heroes = await _context.SuperHeroes.ToListAsync();
             return Ok(heroes);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if(hero is null)
+            {
+                return BadRequest("Hero not found");
+            }
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SuperHero>> AddHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            _context.SaveChanges();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updateHero)
+        {
+            
+            var herodb = await _context.SuperHeroes.FindAsync(updateHero.Id);
+            if (herodb is null)
+            {
+                return BadRequest("Hero not found");
+            }
+            herodb.Name = updateHero.Name;
+            herodb.FirstName = updateHero.FirstName;
+            herodb.LastName = updateHero.LastName;
+            herodb.Place = updateHero.Place;
+
+            _context.SaveChanges();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
     }
 }
