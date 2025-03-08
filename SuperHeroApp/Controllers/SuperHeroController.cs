b1a6cb725ba9830e224a7a138199d8c261a0d72e
shaurbintalib <shaurbintalib515@gmail.com>
2025-03-08 13:40:16 +0800

    superhero add end point functions

---
+++ SuperHeroApp/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroApp.Data;
+using SuperHeroApp.Entities;
+
+namespace SuperHeroApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            return Ok(heroes);
+        }
+    }
+}
---
+++ SuperHeroApp/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroApp.Entities;
+
+namespace SuperHeroApp.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
